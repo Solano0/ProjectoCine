e28cb128d8eefab1ce254db90a32611ed1182fb9
Solano01 <diegosolanonerio@gmail.com>
2024-05-24 21:16:56 -0600

    que agarre el css porfavor ;)

---
+++ api/config.php
@@ -0,0 +1,7 @@
+<?php
+// Constantes para establecer las credenciales de conexión con el servidor de bases de datos.
+define('SERVER', 'localhost');
+define('DATABASE', 'ProjectoCine');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+?>
